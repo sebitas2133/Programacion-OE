996260f81cef43c9ee82110632f2f14396989197
sebitas2133 <bryan.ramirez@correounivalle.edu.co>
2025-02-25 20:24:57 -0500

    clase3

---
+++ unidad1/clase3/clase3.py
@@ -0,0 +1,27 @@
+from tkinter import Frame, Tk
+from tkinter.messagebox import askyesno
+from tkinter import Label
+
+ventanaPrincipal = Tk()
+ventanaPrincipal.title("Prueba de eventos")
+
+def action_click(event):
+    frame.focus_set()
+    print("clicked at", event.x, event.y)
+
+def presionar_tecla(event):
+    print("pressed", repr(event.char))
+
+def el_usuario_quiere_salir():
+    if askyesno("Salir", "¿Está seguro que desea salir?"):
+        ventanaPrincipal.destroy()
+
+frame = Frame(ventanaPrincipal, width=500, height=500)
+frame.bind("<Button-1>", action_click)
+frame.bind("<Key>", presionar_tecla)
+frame.pack()
+
+label = Label(frame, text="Presione una tecla o haga click en la ventana")
+
+ventanaPrincipal.protocol("WM_DELETE_WINDOW", el_usuario_quiere_salir)
+ventanaPrincipal.mainloop()
