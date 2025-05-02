2732bced9b6981591de6aea209d8aaba3e28d596
sebitas2133 <bryan.ramirez@correounivalle.edu.co>
2025-05-02 13:20:50 -0500

    clase11

--- unidad2/clase11/BryanSebastianRamirezHenao/clase 9/frontend/Bryan Sebastian Ramirez.py
+++ unidad2/clase11/BryanSebastianRamirezHenao/clase 9/frontend/Bryan Sebastian Ramirez.py
@@ -3,6 +3,7 @@ import re
 import requests
 from tkinter import messagebox
 from tkinter import Tk
+import tkinter as tk
 
 
 API_URL = "http://127.0.0.1:8000/api/Comida/"
@@ -77,12 +78,12 @@ ventanaPrincipal.title("Comida")
 ventanaPrincipal.geometry("500x400")
 ventanaPrincipal.config(bg="#1E1E1E")
 
-frame = Frame(ventanaPrincipal, width=500, height=500, bg="#1E1E1E")
+frame = Frame(ventanaPrincipal, width=600, height=500, bg="#1E1E1E")
 frame.pack_propagate(False)
 frame.pack()
 
 titulo_label = Label(frame, text="INTRODUZCA LOS SIGUIENTES REQUERIMIENTOS", bg="#1E1E1E", fg="white", height=3, font=("Helvetica", 11, "bold"))
-titulo_label.grid(row=0, column=0, columnspan=2)
+titulo_label.grid(row=0, column=0, columnspan=3)
 
 id = StringVar(frame)
 id_label = Label(frame, text="ID:", bg="#1E1E1E", fg="white")
@@ -101,28 +102,28 @@ nombre_label = Label(frame, text="Nombre:", bg="#1E1E1E", fg="white").grid(row=2
 nombre_entry = Entry(frame, textvariable=nombre)
 nombre_entry.grid(row=2, column=1)
 label_Error_nombre = Label(frame, text="", bg="#1E1E1E", fg="white")
-label_Error_nombre.grid(row=3, column=0, columnspan=2)
+label_Error_nombre.grid(row=2, column=2, pady=2, columnspan=2)
 
 ingrediente_principal = StringVar(frame)
 ingrediente_principal_label = Label(frame, text="Ingrediente principal: ", bg="#1E1E1E", fg="white").grid(row=4, column=0)
 ingrediente_principal_enty = Entry(frame, textvariable=ingrediente_principal)
 ingrediente_principal_enty.grid(row=4, column=1)
 label_Error_ingrediente_principal = Label(frame, text="", bg="#1E1E1E", fg="white")
-label_Error_ingrediente_principal.grid(row=5, column=0, columnspan=2)
+label_Error_ingrediente_principal.grid(row=4, column=2, columnspan=2, pady=2)
 
 calorias = StringVar(frame)
 calorias_label = Label(frame, text="Calorias:", bg="#1E1E1E", fg="white").grid(row=6, column=0)
 calorias_entry = Entry(frame, textvariable=calorias)
 calorias_entry.grid(row=6, column=1)
 label_Error_calorias = Label(frame, text="", bg="#1E1E1E", fg="white")
-label_Error_calorias.grid(row=7, column=0, columnspan=2)
+label_Error_calorias.grid(row=6, column=2, columnspan=2, pady=2)
 
 peso = StringVar(frame)
 peso_label = Label(frame, text="Peso (g):", bg="#1E1E1E", fg="white").grid(row=8, column=0)
 peso_entry = Entry(frame, textvariable=peso)
 peso_entry.grid(row=8, column=1)
 label_Error_peso = Label(frame, text="", bg="#1E1E1E", fg="white")
-label_Error_peso.grid(row=9, column=0, columnspan=2)
+label_Error_peso.grid(row=8, column=2, columnspan=2, pady=2)
 
 
 BotonGuardar = Button(frame, text="Guardar", bg="black", fg="white", width=10,
@@ -134,21 +135,21 @@ BotonGuardar = Button(frame, text="Guardar", bg="black", fg="white", width=10,
                                       calorias.get(), 
                                       peso.get()))
 
-BotonGuardar.grid(row=10, column=0, columnspan=2, pady=5)
+BotonGuardar.grid(row=10, column=0, columnspan=3, pady=5)
 
 BotonActualizar = Button(frame, text="Actualizar", bg="black", fg="white", width=10,
                          activebackground="#1E1E1E",
                          activeforeground="white", bd = 0.5, 
                          command=lambda:actualizar())
 
-BotonActualizar.grid(row=11, column=0, columnspan=2, pady=5)
+BotonActualizar.grid(row=11, column=0, columnspan=3, pady=5)
 
 BotonEliminar = Button(frame, text="Eliminar", bg="black", fg="white", width=10,
                          activebackground="#1E1E1E",
                          activeforeground="white", bd = 0.5, 
                          command=lambda:eliminar())
 
-BotonEliminar.grid(row=12, column=0, columnspan=2, pady=5)
+BotonEliminar.grid(row=12, column=0, columnspan=3, pady=5)
 
 
 
@@ -172,7 +173,7 @@ def evento_presionar_tecla_nombre(event):
     if validarLetras(nombre):
         texto_validar_nombre = ""
     else:
-        texto_validar_nombre = "Solo se permiten letras"
+        texto_validar_nombre = "<- Solo se permiten letras"
     label_Error_nombre.config(text=texto_validar_nombre)
 
 def evento_presionar_tecla_ingrediente(event):
@@ -181,7 +182,7 @@ def evento_presionar_tecla_ingrediente(event):
     if validarLetras(ingrediente_principal):
         texto_validar_ingrediente_principal = ""
     else:
-        texto_validar_ingrediente_principal = "Solo se permiten letras"
+        texto_validar_ingrediente_principal = "<- Solo se permiten letras"
     label_Error_ingrediente_principal.config(text=texto_validar_ingrediente_principal)
 
 def evento_presionar_tecla_calorias(event):
@@ -190,7 +191,7 @@ def evento_presionar_tecla_calorias(event):
     if validarNumeros(calorias):
         texto_validar_calorias = ""
     else:
-        texto_validar_calorias = "Solo se permiten numeros"
+        texto_validar_calorias = "<- Solo se permiten numeros"
     label_Error_calorias.config(text=texto_validar_calorias)
 
 def evento_presionar_tecla_peso(event):
@@ -199,7 +200,7 @@ def evento_presionar_tecla_peso(event):
     if validarNumeros(peso):
         texto_validar_peso = ""
     else:
-        texto_validar_peso = "Solo se permiten numeros"
+        texto_validar_peso = "<- Solo se permiten numeros"
     label_Error_peso.config(text=texto_validar_peso)
 
 nombre_entry.bind("<KeyRelease>", evento_presionar_tecla_nombre)
